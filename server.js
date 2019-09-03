2e45546fa411f7dc2bbaf228d2c7608d32b5cd37
Julie Erlemeier <jmerlemeier@gmail.com>
2019-09-03 10:45:22 -0700

    Worked on server.js camera functionality,

--- server.js
+++ server.js
@@ -5,9 +5,49 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+let multer = require('multer');
+let fs = require('fs');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('./public'));
+
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/fullsize')
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
+var upload = multer({ storage: storage})
+
+
+//=====================
 app.get('/', (request, response) => {
   response.send('Hello Venus.')
 })
+
+//====================== CAMERA FUNCTIONALITY ==================
+app.get('/', renderHome);
+
+function renderHome(request, response) {
+  response.render('index');
+}
+
+// stretch goal
+app.post('/upload', upload.single('image'), function(req, res, next) {
+  res.render('myupload', { image: req.file.path });
+});
+
+app.get('/uploads/fullsize/:file', function(req, res) {
+  file = req.params.file;
+  var img = fs.readFileSync(__dirname + '/uploads/fullsize/' + file);
+  res.writeHead(200, { 'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+});
+
+
 
 app.listen(PORT, () => {console.log(`app is up on port ${PORT}. BYEAH!`)});
 
